a6ef66439c1a3210432f98b9d79bad9506e4ec34
Derek <derekbelloni10@gmail.com>
2022-05-19 11:03:10 -0600

    polished card layout and styling

--- src/Data.js
+++ src/Data.js
@@ -7,5 +7,23 @@ export default [
     endDate: "24 Jan, 2021",
     description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
     imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+  },
+  {
+    title: "Durango",
+    location: "Colorado",
+    googleMapsUrl: "https://www.google.com/maps/place/Durango,+CO/@37.2726634,-107.9373997,12z/data=!3m1!4b1!4m5!3m4!1s0x873c02a6bd85fc23:0xdc2b8882a3a67e5a!8m2!3d37.27528!4d-107.8800667",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan, 2021",
+    description: "Quick facts Durango is a small city in southwestern Colorado, near the New Mexico border. The 19th-century Durango & Silverton Narrow Gauge Railroad steam train passes mountains and canyons. In the city center, its affiliated Railroad Museum displays restored locomotives, aircraft and a baggage car converted into a movie theater. Nearby, the Powerhouse Science Center offers interactive exhibits in a former power plant.",
+    imageUrl: "https://images.unsplash.com/photo-1617738961188-dcd12e1e3d6f?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZHVyYW5nb3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1000"
+  },
+  {
+    title: "Sawtooth Mountains",
+    location: "Idaho",
+    googleMapsUrl: "https://www.google.com/maps/place/Durango,+CO/@37.2726634,-107.9373997,12z/data=!3m1!4b1!4m5!3m4!1s0x873c02a6bd85fc23:0xdc2b8882a3a67e5a!8m2!3d37.27528!4d-107.8800667",
+    startDate: "12 Jan, 2021",
+    endDate: "24 Jan, 2021",
+    description: "Sprawling, scenic set of jagged peaks as high as 10,751 feet offering hiking, fishing & camping.",
+    imageUrl: "https://images.unsplash.com/photo-1624378804429-62b5910a8df4?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=60&raw_url=true&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fHNhd3Rvb3RoJTIwbW91bnRhaW5zfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000"
   }
 ]
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { GrLocation } from 'react-icons/gr';
 
 const Hero = (props) => {
   console.log(props)
@@ -7,9 +7,14 @@ const Hero = (props) => {
     <>
       <div className="destination-container">
         <img className="destination-img" src={props.data.imageUrl} alt="" />
-        <h1 className="destination-title">{props.data.title}</h1>
+        <div className="destination-info">
+          <span><GrLocation />{props.data.location}           <a href="https://goo.gl/maps/1DGM5WrWnATgkSNB8" target="_blank">View on Google Maps</a></span>
+          <h1 className="destination-title">{props.data.title}</h1>
+          <h3>{props.data.startDate} - {props.data.endDate} </h3>
+          <p className="destination-description">{props.data.description}</p>
+        </div>
       </div>
-      <h1>hello</h1>
+
     </>
   )
 }
